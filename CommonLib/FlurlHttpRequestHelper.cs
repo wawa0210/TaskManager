3fc620204a0b31627fa3c2a84ded2f19f0a08296
wawa0210 <xiaozhang0210@hotmail.com>
2018-04-12 23:03:38 +0800

    封装flurl request方法

--- CommonLib/FlurlHttpRequestHelper.cs
+++ CommonLib/FlurlHttpRequestHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace CommonLib
 {
@@ -13,46 +15,122 @@ namespace CommonLib
     public static class FlurlHttpRequestHelper
     {
         #region Get
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T">返回类型</typeparam>
-        /// <param name="requestUrl"></param>
-        /// <param name="queryParam">请求参数</param>
-        /// <returns></returns>
-        public static async Task<T> DoGetAsync<T>(this string requestUrl, object queryParam)
-        {
-            if (string.IsNullOrEmpty(requestUrl)) throw new ArgumentNullException("requestUrl");
-
-            return await requestUrl
-                .SetQueryParams(queryParam)
-                .GetJsonAsync<T>();
-        }
 
         /// <summary>
         /// get请求
         /// </summary>
         /// <param name="requestUrl"></param>
-        /// <param name="queryParam">querystring参数</param>
+        /// <param name="requestEntity"></param>
         /// <returns></returns>
-        public static async Task<string> DoGetAsync(this string requestUrl, object queryParam)
+        public static async Task<ResponseEntity> DoGetAsync(this string requestUrl, RequestEntity requestEntity)
         {
+            var result = new ResponseEntity { RequestEntity = requestEntity };
             if (string.IsNullOrEmpty(requestUrl)) throw new ArgumentNullException("requestUrl");
-            return await requestUrl
-                .SetQueryParams(queryParam)
-                .GetStringAsync();
+            result.RequestBeginTime = DateTime.Now;
+
+            await requestUrl
+               .SetQueryParams(requestEntity.RequestQueryParams)
+               .ConfigureRequest(x =>
+                {
+                    x.AfterCallAsync = async call =>
+                    {
+                        if (call.HttpStatus != null) result.StatusCode = (HttpStatusCode)call.HttpStatus;
+                        result.IsSucceed = call.Succeeded;
+                        result.Exception = call.Exception;
+                        result.RequestBody = await call.Response.Content.ReadAsStringAsync();
+                        result.RequestEndTime = DateTime.Now;
+                    };
+                    x.OnError = callBack => callBack.ExceptionHandled = true;
+                })
+               .GetStringAsync();
+
+            return result;
         }
         #endregion
 
 
         #region Post
+
+        /// <summary>
+        /// get请求
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <param name="requestEntity"></param>
+        /// <returns></returns>
+        public static async Task<ResponseEntity> DoPostAsync(this string requestUrl, RequestEntity requestEntity)
+        {
+            var result = new ResponseEntity { RequestEntity = requestEntity };
+            if (string.IsNullOrEmpty(requestUrl)) throw new ArgumentNullException("requestUrl");
+            result.RequestBeginTime = DateTime.Now;
+            await requestUrl
+               .SetQueryParams(requestEntity.RequestQueryParams)
+               .ConfigureRequest(x =>
+               {
+                   x.AfterCallAsync = async call =>
+                   {
+                       if (call.HttpStatus != null) result.StatusCode = (HttpStatusCode)call.HttpStatus;
+                       result.IsSucceed = call.Succeeded;
+                       result.Exception = call.Exception;
+                       result.RequestBody = await call.Response.Content.ReadAsStringAsync();
+                       result.RequestEndTime = DateTime.Now;
+                   };
+                   x.OnError = callBack => callBack.ExceptionHandled = true;
+               })
+               .PostJsonAsync(requestEntity.RequestBody);
+
+            return result;
+        }
         #endregion
     }
 
+    /// <summary>
+    /// 请求信息
+    /// </summary>
     public class RequestEntity
     {
+        /// <summary>
+        /// 请求地址
+        /// </summary>
         public string RequestUrl { get; set; }
 
-        public object requestHeaders { get; set; }
+        /// <summary>
+        /// 请求头
+        /// </summary>
+
+        public object RequestHeaders { get; set; }
+
+        /// <summary>
+        /// query string 参数
+        /// </summary>
+
+        public object RequestQueryParams { get; set; }
+
+        /// <summary>
+        /// 请求body
+        /// </summary>
+        public object RequestBody { get; set; }
+    }
+
+    /// <summary>
+    /// http响应信息
+    /// </summary>
+    public class ResponseEntity
+    {
+        public RequestEntity RequestEntity { get; set; }
+
+        public DateTime RequestBeginTime { get; set; }
+
+        public DateTime RequestEndTime { get; set; }
+
+        public string RequestBody { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        /// <summary>
+        /// 请求异常信息
+        /// </summary>
+        public Exception Exception { get; set; }
+
+        public bool IsSucceed { get; set; }
     }
 }
